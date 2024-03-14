2b25d09172804ada548b99956139634eed648bd3
Saksham Singhania <151566210+sakshamfr@users.noreply.github.com>
2024-03-14 23:57:33 +0530

    Add files via upload

---
+++ merge.js
@@ -0,0 +1,60 @@
+function merge(arr, l, m, r) {
+    let n1 = m - l + 1;
+    let n2 = r - m;
+
+    let L = new Array(n1);
+    let R = new Array(n2);
+
+    for (let i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (let j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    let i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+function mergeSort(arr, l, r) {
+    if (l < r) {
+        let m = Math.floor((l + r) / 2);
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+function printArray(arr) {
+    console.log(arr.join(" "));
+}
+
+let arr = [64, 25, 12, 22, 11, 90];
+console.log("Given array:");
+printArray(arr);
+
+mergeSort(arr, 0, arr.length - 1);
+
+console.log("Sorted array:");
+printArray(arr);
